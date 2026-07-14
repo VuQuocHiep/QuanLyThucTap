1c76922226e4f925ead6095f361dc6cdcc36df14
Vu Quoc Hiep <vuquochiep2005@gmail.com>
2026-07-14 20:36:50 +0700

    Initial commit

---
+++ Backend/B23DCCN295 (2)/B23DCCN295/src/main/java/com/Hiep/B23DCCN295/B23Dccn295Application.java
@@ -0,0 +1,13 @@
+package com.Hiep.B23DCCN295;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B23Dccn295Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B23Dccn295Application.class, args);
+	}
+
+}
---
+++ Backend/B23DCCN295 (2)/B23DCCN295/src/test/java/com/Hiep/B23DCCN295/B23Dccn295ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hiep.B23DCCN295;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B23Dccn295ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
